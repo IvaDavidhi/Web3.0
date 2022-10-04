fbd1ba1bd891c188cad29984de21caeb63083dbf
Iva Davidhi <iva.davidhi@gmail.com>
2022-10-04 22:39:09 +0200

    create TransactionContext

---
+++ client/src/utils/constants.js
@@ -0,0 +1,5 @@
+import abi from "./Transactions.json";
+
+export const contractABI = abi.abi;
+
+export const contractAddress = "0x1D7Fd62D3FeE305Ee635f4B6B8cC62779B75c3F4";
--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,6 +1,15 @@
-require("@nomicfoundation/hardhat-toolbox");
+//https://eth-goerli.g.alchemy.com/v2/UFtuXdWQ2XLNb8CwJofM1Hzu5KX9IUTb
 
-/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomiclabs/hardhat-waffle");
+
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.0",
+  networks: {
+    goerli: {
+      url: "https://eth-goerli.g.alchemy.com/v2/UFtuXdWQ2XLNb8CwJofM1Hzu5KX9IUTb",
+      accounts: [
+        "9d0d171b46c6b37a72e29170ae9f4611a11254d7a1a4ee300ab6118e5a53c8ea",
+      ],
+    },
+  },
 };
