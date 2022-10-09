92b3c9e153f02d09b06f2421260f946fb87fc7bb
Iva Davidhi <iva.davidhi@gmail.com>
2022-10-09 21:30:35 +0200

    update hardhat.config

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,15 +1,14 @@
-//https://eth-goerli.g.alchemy.com/v2/UFtuXdWQ2XLNb8CwJofM1Hzu5KX9IUTb
-
 require("@nomiclabs/hardhat-waffle");
+
+const RPC_URL = process.env.RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 module.exports = {
   solidity: "0.8.0",
   networks: {
     goerli: {
-      url: "https://eth-goerli.g.alchemy.com/v2/UFtuXdWQ2XLNb8CwJofM1Hzu5KX9IUTb",
-      accounts: [
-        "9d0d171b46c6b37a72e29170ae9f4611a11254d7a1a4ee300ab6118e5a53c8ea",
-      ],
+      url: RPC_URL,
+      accounts: [PRIVATE_KEY],
     },
   },
 };
